f5a68ab6d2290dd9364cda42490a64d2f2574c74
rober0xf <robertinopili13@gmail.com>
2024-12-17 20:54:41 -0300

    constants header

---
+++ include/constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+#define CELL_SIZE 10
+#define TITLE "game of life"
+#define WIDTH 900
+#define HEIGHT 600
+#define rows (HEIGHT / CELL_SIZE)
+#define cols (WIDTH / CELL_SIZE)
+
+#endif // !CONSTANTS_H_
